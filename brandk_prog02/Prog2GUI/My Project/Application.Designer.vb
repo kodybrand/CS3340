cda7cdcef9a141a39bf63683948fdaf48db40d9d
Kody Brand <kodybrand@gmail.com>
2016-02-04 16:55:40 -0600

    Set up workspace for prog02

--- brandk_prog02/Prog2GUI/My Project/Application.Designer.vb
+++ brandk_prog02/Prog2GUI/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Prog2GUI.Form1
+            Me.MainForm = Global.Prog2GUI.FormClassHouse
         End Sub
     End Class
 End Namespace
